5bbfa844efcd0ec12e4ddae391fce3abc56d3fa5
mydeathtrial <mydeathtrial@163.com>
2017-05-19 11:41:02 +0800

    调整配置文件

---
+++ src/main/java/com/agile/common/util/CacheUtil.java
@@ -0,0 +1,40 @@
+package com.agile.common.util;
+
+import com.agile.common.base.AbstractCacheUtil;
+
+/**
+ * Created by 佟盟 on 2017/5/19
+ */
+public class CacheUtil {
+
+    public static AbstractCacheUtil cache;
+
+    public static AbstractCacheUtil getCache(){
+        if (ObjectUtil.isEmpty(cache)){
+            String cacheProperty = "agile.cache.proxy";
+            cache = (AbstractCacheUtil) FactoryUtil.getBean(PropertiesUtil.getProperties(cacheProperty)+"Service");
+        }
+        return cache;
+    }
+
+    public static void setCache(String key, Object object) {
+        getCache().setCache(key, object);
+    }
+
+    public static void setCache(String key, Object object, int timeToIdleSeconds) {
+        getCache().setCache(key, object, timeToIdleSeconds);
+
+    }
+
+    public static void removeCache(String key) {
+        getCache().removeCache(key);
+    }
+
+    public static void removeAll() {
+        getCache().removeAll();
+    }
+
+    public static Object getCache(String key) {
+        return getCache().getCache(key);
+    }
+}
